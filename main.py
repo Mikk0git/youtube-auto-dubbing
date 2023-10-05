02abe9af89cc908e451151ae500c10ae28171233
Mikk0git <kasprzakmilan1@gmail.com>
2023-10-05 15:53:10 +0200

    get srt from user

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("Youtube Auto Dubbing")
 
+def main():
+    print("Youtube Auto Dubbing")
+
+    try:
+        srtFileDir = input("Enter the path of the srt file: ")
+        srtFile = open(srtFileDir, "r")
+    except FileNotFoundError:
+        print("File not found")
+        return
+
+    generateAudio(srtFile)
+
+
+def generateAudio(srtFile):
+    for line in srtFile:
+        print(line)
+
+
+main()
